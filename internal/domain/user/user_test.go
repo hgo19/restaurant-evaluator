f395524bee4cf3dcd887c0de35d1f8dfcaee2800
Hugo Leonardo <huleonardo19@gmail.com>
2024-06-20 19:56:20 -0300

    test: ensure user entity fields is validate with the right erros when it was created

--- internal/domain/user/user_test.go
+++ internal/domain/user/user_test.go
@@ -7,10 +7,11 @@ import (
 )
 
 var (
-	username = "user test"
-	email    = "foo@bar.com"
-	password = "testPassword1@"
-	token    = "token_for_test"
+	username      = "user test"
+	email         = "foo@bar.com"
+	password      = "testPassword1@"
+	token         = "token_for_test"
+	validUserType = "Consumer"
 )
 
 func Test_NewUser_CustomerSuccessCase(t *testing.T) {
@@ -42,4 +43,40 @@ func Test_NewUser_InvalidUserType(t *testing.T) {
 
 	assert.Error(err)
 	assert.EqualError(err, "invalid user type")
+}
+
+func Test_NewUser_InvalidUsername(t *testing.T) {
+	assert := assert.New(t)
+	invalidUsername := ""
+
+	_, err := NewUser(invalidUsername, email, password, validUserType, token)
+
+	assert.EqualError(err, "Username is required with min 5")
+}
+
+func Test_NewUser_InvalidEmail(t *testing.T) {
+	assert := assert.New(t)
+	invalidEmail := "invalid_email"
+
+	_, err := NewUser(username, invalidEmail, password, validUserType, token)
+
+	assert.EqualError(err, "Email is invalid")
+}
+
+func Test_NewUser_InvalidPasswordHash(t *testing.T) {
+	assert := assert.New(t)
+	invalidPasswordHash := ""
+
+	_, err := NewUser(username, email, invalidPasswordHash, validUserType, token)
+
+	assert.EqualError(err, "PasswordHash is required")
+}
+
+func Test_NewUser_InvalidToken(t *testing.T) {
+	assert := assert.New(t)
+	invalidToken := ""
+
+	_, err := NewUser(username, email, password, validUserType, invalidToken)
+
+	assert.EqualError(err, "Token is required")
 }
