79c4fb1e92f9ba5b7a767d92094e610b65fd6eec
Hugo Leonardo <huleonardo19@gmail.com>
2024-06-23 21:48:25 -0300

    feat: improves service with a token generator inside

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -1,15 +1,24 @@
 package user
 
 import (
+	"errors"
+	"restaurant-evaluator/internal/adapters"
 	"restaurant-evaluator/internal/dto"
 )
 
 type Service struct {
-	Repository Repository
+	Repository     Repository
+	TokenGenerator adapters.TokenAdapter
 }
 
 func (s *Service) Create(newUser dto.NewUser) (string, error) {
-	user, err := NewUser(newUser.Username, newUser.Email, newUser.PasswordHash, newUser.UserType, newUser.Token)
+	token, err := s.TokenGenerator.GenerateToken(newUser.Username, newUser.Email)
+
+	if err != nil {
+		return "", errors.New("token not generated")
+	}
+
+	user, err := NewUser(newUser.Username, newUser.Email, newUser.PasswordHash, newUser.UserType, token)
 	if err != nil {
 		return "", err
 	}
@@ -19,5 +28,5 @@ func (s *Service) Create(newUser dto.NewUser) (string, error) {
 		return "", err
 	}
 
-	return user.ID, nil
+	return user.Token, nil
 }
