40119ff09bef861ba842ef743191bae12e3c19f8
Hugo Leonardo <huleonardo19@gmail.com>
2024-06-27 19:57:42 -0300

    feat: ensure user entity was called with the hashed password

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -1,17 +1,26 @@
 package user
 
 import (
+	"restaurant-evaluator/internal/adapters"
 	"restaurant-evaluator/internal/dto"
 	internalerrors "restaurant-evaluator/internal/internal-errors"
 )
 
 type Service struct {
 	Repository Repository
+	Encrypter  adapters.Encrypter
 }
 
 func (s *Service) Create(newUser dto.NewUser) (string, error) {
 
-	user, err := NewUser(newUser.Username, newUser.Email, newUser.PasswordHash, newUser.UserType)
+	passwordHash, err := s.Encrypter.HashPassword(newUser.Password)
+
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
+
+	user, err := NewUser(newUser.Username, newUser.Email, passwordHash, newUser.UserType)
+
 	if err != nil {
 		return "", err
 	}
