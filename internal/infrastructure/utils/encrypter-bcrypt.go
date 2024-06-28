96905f9d1507c5d024b39544428bec70b7651f9b
Hugo Leonardo <huleonardo19@gmail.com>
2024-06-27 21:52:29 -0300

    feat: ensure encrypter-bcrypt implements encrypter interface

---
+++ internal/infrastructure/utils/encrypter-bcrypt.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type EncrypterBcrypt struct{}
+
+func (e EncrypterBcrypt) HashPassword(password string) (string, error) {
+	saltRound, err := strconv.Atoi(os.Getenv("SALT_ROUNDS"))
+	if err != nil {
+		println("Error to convert .env salt_rounds to int")
+	}
+
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), saltRound)
+	return string(bytes), err
+}
