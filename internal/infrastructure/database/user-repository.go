8ad407f78a82431e82dae73a0d687ee3c79db80e
Hugo Leonardo <huleonardo19@gmail.com>
2024-07-13 00:14:32 -0300

    feat: create findByEmail method in repository in memory

--- internal/infrastructure/database/user-repository.go
+++ internal/infrastructure/database/user-repository.go
@@ -10,3 +10,13 @@ func (r *UserRepository) Save(user *user.User) error {
 	r.users = append(r.users, *user)
 	return nil
 }
+
+func (r *UserRepository) FindByEmail(email string) (*user.User, error) {
+	for i := range r.users {
+		if r.users[i].Email == email {
+			return &r.users[i], nil
+		}
+	}
+
+	return nil, nil
+}
