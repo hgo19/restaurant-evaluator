c85ef7e875747ad047f40306261e29c5b48e012d
Hugo Leonardo <huleonardo19@gmail.com>
2024-07-13 00:04:26 -0300

    feat: creates GenerateToken method in user service

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -9,6 +9,7 @@ import (
 type Service struct {
 	Repository Repository
 	Encrypter  adapters.Encrypter
+	TokenAuth  adapters.TokenAuth
 }
 
 func (s *Service) Create(newUser dto.User) (string, error) {
@@ -31,4 +32,23 @@ func (s *Service) Create(newUser dto.User) (string, error) {
 	}
 
 	return user.ID, nil
+}
+
+func (s *Service) GenerateToken(email string) (string, error) {
+	user, err := s.Repository.FindByEmail(email)
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
+
+	if user == nil {
+		return "", internalerrors.NotFound
+	}
+
+	token, err := s.TokenAuth.Generate(user.Email)
+
+	if err != nil {
+		return "", internalerrors.ErrInternal
+	}
+
+	return token, nil
 }
